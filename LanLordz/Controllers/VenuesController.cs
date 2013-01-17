07ccabd79a644d4e7151dc76c844ac134f8b8b8e
John Gietzen <otac0n@gietzen.us>
2013-01-17 08:38:58 -0800

    Added the ability to create venues.

---
+++ LanLordz/Controllers/VenuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanLordz.Models;
+using System.Data.Common;
+
+namespace LanLordz.Controllers
+{
+    public class VenuesController : AdminControllerBase
+    {
+        public ActionResult Index()
+        {
+            var venues = this.Db.Venues.ToList();
+            return View(venues);
+        }
+
+        public ActionResult Create()
+        {
+            return View("Edit");
+        }
+
+        [HttpPost]
+        public ActionResult Create(Venue venue)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return View("Edit");
+            }
+
+            try
+            {
+                this.Db.Venues.InsertOnSubmit(venue);
+                this.Db.SubmitChanges();
+            }
+            catch (DbException ex)
+            {
+                ModelState.AddModelError("", ex.GetBaseException().Message);
+                return View("EditSponsor");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
